c680134e5129dce3b43f683c3dbff42fa3d40952
梁良 <liangliang@icourt.cc>
2018-11-16 19:50:26 +0800

    feat: unirest update

--- src/main/java/com/us/improve/unirest/MainTest.java
+++ src/main/java/com/us/improve/unirest/MainTest.java
@@ -1,12 +1,6 @@
 package com.us.improve.unirest;
 
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author liangliang
@@ -15,30 +9,7 @@ import java.util.Map;
 public class MainTest {
 
     public static void main(String[] args) throws UnirestException {
-        HttpResponse<JsonNode> jsonResponse = Unirest.post("http://httpbin.org/post")
-                .header("accept", "application/json")
-                .queryString("apiKey", "123")
-                .field("parameter", "value")
-                .field("foo", "bar")
-                .asJson();
-
-        System.out.println(jsonResponse.getStatus());
-
-        Map<String, String> headers = new HashMap();
-        headers.put("accept", "application/json");
-
-        Map<String, Object> fields = new HashMap();
-        fields.put("parameter", "value");
-        fields.put("foo", "bar");
-
-        jsonResponse = Unirest.post("http://httpbin.org/post")
-                .headers(headers)
-                .queryString("apiKey", "123")
-                .fields(fields)
-                .asJson();
-
-        System.out.println(jsonResponse.getStatus());
-
+        
     }
 
 }
