5bfd3cb4d2e558f4bdde82f2f0f1bb124a90e369
梁良 <liangliang@icourt.cc>
2018-11-16 21:07:26 +0800

    feat: unirest update

--- src/main/java/com/us/improve/unirest/MainTest.java
+++ src/main/java/com/us/improve/unirest/MainTest.java
@@ -1,5 +1,8 @@
 package com.us.improve.unirest;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 /**
@@ -9,7 +12,18 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 public class MainTest {
 
     public static void main(String[] args) throws UnirestException {
+        HttpResponse<JsonNode> jsonResponse = Unirest.post("http://httpbin.org/post")
+            .header("accept", "application/json")
+            .queryString("apiKey", "123")
+            .field("parameter", "value")
+            .field("foo", "bar")
+            .asJson();
 
+        System.out.println(jsonResponse.getStatus());
+        System.out.println(jsonResponse.getStatusText());
+        System.out.println(jsonResponse.getHeaders());
+        System.out.println(jsonResponse.getBody());
+        System.out.println(jsonResponse.getRawBody());
     }
 
 }
