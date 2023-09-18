2845f91047d1e1ced6d134c06a217bc87f5f14d7
Loren <liangliangpro@outlook.com>
2023-09-18 21:28:17 +0800

    feat: I am back

--- src/main/java/com/us/improve/uuid/MainTest.java
+++ src/main/java/com/us/improve/uuid/MainTest.java
@@ -14,6 +14,7 @@ public class MainTest {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(UUID.randomUUID().toString());
+            System.out.println("I am back");
         }
     }
 
