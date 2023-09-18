c3a335a3cb2bafd1d60b1d50ba2879b8c0dcecfe
Loren <liangliangpro@outlook.com>
2023-09-19 00:37:12 +0800

    test: git

---
+++ src/main/java/com/us/improve/git/MainTest.java
@@ -0,0 +1,15 @@
+package com.us.improve.git;
+
+/**
+ * @Author loren
+ * @Description TODO
+ * @Date 2023-09-19 00:36
+ * @Version 1.0
+ **/
+public class MainTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Git.");
+	}
+
+}
